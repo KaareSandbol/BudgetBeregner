4ce3fdbfeaefd4de8b31dda9bbfc09b754f9a068
math964g <math964g@edu.eal.dk>
2018-11-27 10:11:59 +0100

    Copied dragons lair menu

---
+++ Program/Budget Beregner/Menu.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget_Beregner
+{
+    public class Menu
+    {
+        public void Show()
+        {
+            bool running = true;
+            do
+            {
+                ShowMenu();
+                string choice = GetUserChoice();
+                switch (choice)
+                {
+                    case "0":
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ugyldigt valg.");
+                        Console.ReadLine();
+                        break;
+                }
+            } while (running);
+        }
+
+        private void ShowMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Dragons Lair");
+            Console.WriteLine();
+            Console.WriteLine("1. Præsenter turneringsstilling");
+            Console.WriteLine("2. Planlæg runde i turnering");
+            Console.WriteLine("3. Registrér afviklet kamp");
+            Console.WriteLine("4. Opret ny turnering.");
+            Console.WriteLine("5. Tilføj hold til en turnering.");
+            Console.WriteLine("6. Vis alle turneringer");
+            Console.WriteLine("0. Exit");
+        }
+
+        private string GetUserChoice()
+        {
+            Console.WriteLine();
+            Console.Write("Indtast dit valg: ");
+            return Console.ReadLine();
+        }
+    }
+}
--- Program/Budget Beregner/Program.cs
+++ Program/Budget Beregner/Program.cs
@@ -10,6 +10,14 @@ namespace Budget_Beregner
     {
         static void Main(string[] args)
         {
+            Program myProgram = new Program();
+            myProgram.Run();
+        }
+
+        void Run()
+        {
+            Menu menu = new Menu();
+            menu.Show();
         }
     }
 }
