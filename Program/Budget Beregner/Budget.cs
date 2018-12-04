1e02b2eca945e381d74dfe5561316fd47c27d832
math964g <math964g@edu.eal.dk>
2018-12-04 15:27:54 +0100

    Works but it's not pretty

--- Program/Budget Beregner/Budget.cs
+++ Program/Budget Beregner/Budget.cs
@@ -9,15 +9,17 @@ namespace Budget_Beregner
 {
     class Budget
     {
+        // TODO: Need variable to check if it's expense or income.
         int row = 10;
         int column = 2;
         bool exit = true;
+        // HACK: Hacked for demo too.
         int columnamount = 4;
 
         List<int> Income = new List<int>();
         List<int> Expenses = new List<int>();
 
-        private void Input()
+        private void InputIncome()
         {
             while (exit)
             {
@@ -28,13 +30,40 @@ namespace Budget_Beregner
                     column += 1;
                     exit = false;
                 }
+
                 catch (Exception)
                 {
-                    Console.SetCursorPosition(0, column + 8);
+                    Console.SetCursorPosition(row + 10, column);
                     Console.WriteLine("Indtast venligst et tal.");
                     Console.ReadKey();
-                    Console.SetCursorPosition(0, column + 8);
-                    Console.Write(new string(' ', Console.LargestWindowWidth));
+                    Console.SetCursorPosition(row + 10, column);
+                    Console.Write("                                    ");
+                    Console.SetCursorPosition(row, column);
+                    Console.Write("                   ");
+                    Console.SetCursorPosition(row, column);
+                }
+            }
+        }
+
+        private void InputExpense()
+        {
+            while (exit)
+            {
+                try
+                {
+                    Console.SetCursorPosition(row, column);
+                    Expenses.Add(int.Parse(Console.ReadLine()));
+                    column += 1;
+                    exit = false;
+                }
+
+                catch (Exception)
+                {
+                    Console.SetCursorPosition(row + 10, column);
+                    Console.WriteLine("Indtast venligst et tal.");
+                    Console.ReadKey();
+                    Console.SetCursorPosition(row + 10, column);
+                    Console.Write("                                    ");
                     Console.SetCursorPosition(row, column);
                     Console.Write("                   ");
                     Console.SetCursorPosition(row, column);
@@ -53,9 +82,24 @@ namespace Budget_Beregner
             Console.WriteLine("Husleje: ");
             // Console.WriteLine("Personlig pleje: ");
 
+            // HACK: Only works if there's exactly 2 types of income. Hacked for demo purposes.
             for (int i = 0; i < columnamount; i++)
             {
-                Input();
+                if (i < 2)
+                {
+                    InputIncome();
+                }
+              
+                if (i == 2)
+                {
+                    column += 2;
+                }
+
+                if (i >= 2)
+                {
+                    InputExpense();
+                }
+
                 exit = true;
             }
           
