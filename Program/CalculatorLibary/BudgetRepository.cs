51413fb00dec7d60de5d88d514e700b6d2687b33
Lasse Vestergaard Fuglsbjerg <lass7016@edu.eal.dk>
2018-12-04 14:14:28 +0100

    Moved Calculator BudgetRepo

---
+++ Program/CalculatorLibary/BudgetRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibary
+{
+    public class BudgetRepository
+    {
+        public int CalculatorDisposable(List<int> IncomeList, List<int> ExpensesList)
+        {
+            int incomeSum = IncomeList.Sum();
+            int expensesSum = ExpensesList.Sum();
+            return incomeSum - expensesSum;
+        }
+    }
+}
