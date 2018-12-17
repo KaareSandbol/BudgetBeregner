c383221ceba60aacdcc73d5b5f1fb0d9e1a9a65d
math964g <math964g@edu.eal.dk>
2018-12-17 07:38:48 +0100

    New design variant

--- Program/CalculatorLibary/Design.cs
+++ Program/CalculatorLibary/Design.cs
@@ -6,8 +6,27 @@ using System.Threading.Tasks;
 
 namespace BudgetLibrary
 {
+    public static class StringExtentions
+    {
+        public static string PadBoth(string source, int length)
+        {
+            int spaces = length - source.Length;
+            int padLeft = spaces / 2 + source.Length;
+            return source.PadLeft(padLeft).PadRight(length);
+
+        }
+    }
+
     public class Design
     {
+        public static string PadBoth(string source, int length)
+        {
+            int spaces = length - source.Length;
+            int padLeft = spaces / 2 + source.Length;
+            return source.PadLeft(padLeft).PadRight(length);
+
+        }
+
         public static void FrameLeft(string mystring)
         {
             Console.WriteLine("| "+mystring.PadLeft(10, ' '));
@@ -15,7 +34,7 @@ namespace BudgetLibrary
 
         public static void Padding(string mystring)
         {
-            Console.WriteLine("  |  " + mystring.PadRight(22, ' ') + "|");
+            Console.WriteLine("  |  " + PadBoth(mystring, 22) + "|");
         }
 
         public void FrameRight(string mystring)
